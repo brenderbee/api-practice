b895da1b6ffe2eae72469e50d9fe3e3ed26b4b5d
Brenda Franco and Kayl Eubanks <brenda@>
2018-04-24 15:25:38 -0700

    Stuck on returning proper value to test variable

--- spec/scripts-spec.js
+++ spec/scripts-spec.js
@@ -1,10 +1,22 @@
 import { API } from './../src/scripts.js';
+import { parseResult } from './../src/scripts.js';
 
 describe('API Test', function() {
 
   it('should return a variable containing the API response', function() {
     const newAPI = new API();
     let test = 0;
-    expect(test=newAPI.APIRequest()).not.toEqual(0);
+    test = newAPI.APIRequest();
+    console.log(test);
+    expect(test).not.toEqual(0);
   });
+
+  // it('should return a string containing all dinos returned by the API', function() {
+  //   const newAPI = new API();
+  //   let test = newAPI.APIRequest();
+  //   console.log(test);
+  //   let output = parseResult(test);
+  //   expect(output).not.toEqual(0);
+  // });
+
 });
--- src/scripts.js
+++ src/scripts.js
@@ -1,23 +1,52 @@
+export function parseResult(arrayOfArrays) {
+  console.log(arrayOfArrays);
+  let dinoStrings = [];
+  let outputString = "";
+  for (var i = 0; i < arrayOfArrays.length; i++) {
+    dinoStrings.push(arrayOfArrays[i].join(' '));
+  }
+  // arrayOfArrays.forEach(function(array){
+  //   dinoStrings.push(array.join(' '));
+  // });
+  outputString = dinoStrings.join(' ');
+  return outputString;
+}
+
 class API {
 
-
-
   APIRequest() {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=2&words=30');
-    xhr.onload = function() {
-    	if (this.status === 200) {
-    		var responseObject = this.response;
-    		console.log(this.response);
-    		console.log(responseObject);
-    		return responseObject;
-        } else {
-    		console.log('FUUUUUUUUUU');
-        }
-    };
-    xhr.send();
+    let promise = new Promise(function(resolve, reject) {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function() {
+        if (this.status === 200) {
+          console.log("Successful call " + xhr.response);
+          console.log("type of response" + xhr.responseType);
+          resolve(xhr.response);
+          } else {
+          console.log('FUUUUUUUUUU');
+          reject(Error(xhr.statusText));
+          }
+      }
+      xhr.open('GET', 'http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=2&words=30');
+      xhr.send();
+    });
+    let returnValue = promise.then(resolve);
+    console.log("after promise " + returnValue);
+    return returnValue;
+    // const xhr = new XMLHttpRequest();
+    // let responseObject = '';
+    // xhr.open('GET', 'http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=2&words=30');
+    // xhr.onload = function() {
+    // 	if (this.status === 200) {
+    //     console.log(this.response);
+    // 		responseObject = this.response;
+    //     } else {
+    // 		console.log('FUUUUUUUUUU');
+    //     }
+    //   return responseObject;
+    // };
+    // xhr.send();
   }
-
 }
 
 export { API };
