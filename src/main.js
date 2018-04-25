475e4c1f3d77ff1e3e793b5a539c7dfdd91b418e
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-04-24 19:24:27 -0700

    Add front end to get dino api to function.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,37 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-$(document).ready(function(){
+// import {parseResult} from './scripts.js';
 
+$(document).ready(function(){
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+    let inputParagraph = parseInt($("#paragraph").val());
+    let inputWords = parseInt($("#word").val());
+
+    let promise = new Promise(function(resolve, reject) {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', `http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=${inputParagraph}&words=${inputWords}`);
+      xhr.send();
+      xhr.onload = function() {
+        if (this.status === 200) {
+          resolve(xhr.response);
+          } else {
+          reject(Error(xhr.statusText));
+          }
+      }
+    });
+
+    promise.then(function(response) {
+      let arrays = JSON.parse(response);
+      arrays.forEach(function(array) {
+        let string = array.join(' ');
+        $(".outputDinos").append('<p>' + string + '</p>');
+      });
+    }, function(error) {
+      $(".outputDinos").append("There has been an error!: " + error);
+    });
+
+  });
 });
--- src/scripts.js
+++ src/scripts.js
@@ -1,42 +1,38 @@
-export function parseResult(arrayOfArrays) {
-  console.log(arrayOfArrays);
-  let dinoStrings = [];
-  let outputString = "";
-  for (var i = 0; i < arrayOfArrays.length; i++) {
-    dinoStrings.push(arrayOfArrays[i].join(' '));
-  }
-  // arrayOfArrays.forEach(function(array){
-  //   dinoStrings.push(array.join(' '));
-  // });
-  outputString = dinoStrings.join(' ');
-  return outputString;
-}
+// export function parseResult(arrayOfArrays) {
+//   let dinoStrings = [];
+//   let outputString = "";
+//   for (var i = 0; i < arrayOfArrays.length; i++) {
+//     dinoStrings.push(arrayOfArrays[i].join(' '));
+//   }
+//   outputString = dinoStrings.join(' ');
+//   return outputString;
+// }
 
-class API {
-
-  APIRequest() {
-    let promise = new Promise(function(resolve, reject) {
-      const xhr = new XMLHttpRequest();
-      xhr.open('GET', 'http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=2&words=30');
-      xhr.send();
-      xhr.onload = function() {
-        if (this.status === 200) {
-          resolve(xhr.response);
-          } else {
-          console.log('FUUUUUUUUUU');
-          reject(Error(xhr.statusText));
-          }
-      }
-    });
-    let dino = promise.then(function(response) {
-      let output = JSON.parse(response);
-      console.log("after promise" + output);
-      return output;
-    }, function(error) {
-      alert("There has been an error!: " + error);
-    });
-    return dino;
-  }
-}
-
-export { API };
+// class API {
+//
+//   APIRequest() {
+//     let promise = new Promise(function(resolve, reject) {
+//       const xhr = new XMLHttpRequest();
+//       xhr.open('GET', 'http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=2&words=30');
+//       xhr.send();
+//       xhr.onload = function() {
+//         if (this.status === 200) {
+//           resolve(xhr.response);
+//           } else {
+//           console.log('FUUUUUUUUUU');
+//           reject(Error(xhr.statusText));
+//           }
+//       }
+//     });
+//     let dino = promise.then(function(response) {
+//       let output = JSON.parse(response);
+//       console.log("after promise" + output);
+//       return output;
+//     }, function(error) {
+//       alert("There has been an error!: " + error);
+//     });
+//     return dino;
+//   }
+// }
+//
+// export { API };
